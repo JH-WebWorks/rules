16a6719cbaaed2df77a3a265702418389c4acd33
Keiwan Jamaly <keiwanjamaly@gmail.com>
2021-04-05 13:34:07 +0200

    minimal version of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,8 @@
 module.exports = {
     extends: [
         'eslint:recommended',
-        'plugin:import/errors',
-        'plugin:react/recommended',
-        'plugin:prettier/recommended',
-        'prettier/react',
     ],
     plugins: [
-        'react-hooks',
     ],
     rules: {
     },
