5d5576dfa3f19d538ef7f43b54ea3a5bac77f3e7
Keiwan Jamaly <keiwanjamaly@gmail.com>
2021-04-05 13:31:28 +0200

    removed plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
         'eslint:recommended',
         'plugin:import/errors',
         'plugin:react/recommended',
-        'plugin:jsx-a11y/recommended',
         'plugin:prettier/recommended',
         'prettier/react',
     ],
