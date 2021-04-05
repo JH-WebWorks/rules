70632b5164b2858f6e6cd7a135ebe6078878c458
Keiwan Jamaly <keiwanjamaly@gmail.com>
2021-04-05 13:40:59 +0200

    added typescript support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
     extends: [
         'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
     ],
     plugins: [
+        '@typescript-eslint',
     ],
     rules: {
     },
+    parser: '@typescript-eslint/parser',
 };
