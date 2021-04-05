2255b84854e16d59b341f27b592542f36a5b09dc
Keiwan Jamaly <keiwanjamaly@gmail.com>
2021-04-05 15:29:07 +0200

    addd rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,18 @@ module.exports = {
         '@typescript-eslint',
     ],
     rules: {
+        "no-console": "error",
+        "no-promise-executor-return": "warn",
+        "default-case": "warn",
+        "default-case-last": "error",
+        "default-param-last": "error",
+        "eqeqeq": "error",
+        "grouped-accessor-pairs": "error",
+        "no-empty-function": "error",
+        "no-eval": "error",
+        "no-implicit-coercion": "warn",
+        "no-multi-spaces": "warn",
+        "no-param-reassign": "error"
     },
     parser: '@typescript-eslint/parser',
 };
